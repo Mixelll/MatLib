f5ebc9fa1ce9c916cfcb5ba1f006a72f3d8120f0
Mixelll <65568239+Mixelll@users.noreply.github.com>
2021-05-22 22:35:04 +0200

    REORDER

--- SOMELINES.m
+++
@@ -1,3 +0,0 @@
-ff1200 = textread('C:\Users\admin2\Downloads\1200C6Hz 0.1PLC.txt','');figure;plot(ff1200(:,1))
-ff1400 = textread('C:\Users\admin2\Downloads\1400C6Hz 0.1PLC.txt','');figure;plot(ff1400(:,1))
-ff1600 = textread('C:\Users\admin2\Downloads\1600C6Hz 0.1PLC.txt','');figure;plot(ff1600(:,1))
